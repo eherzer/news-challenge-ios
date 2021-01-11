6aff0f5ac045271ad31f636506cd080b58f120d0
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-01-11 20:55:28 -0300

    Criação do projeto

---
+++ News Challenge/News Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Challenge
+//
+//  Created by Eduardo Herzer on 11/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
