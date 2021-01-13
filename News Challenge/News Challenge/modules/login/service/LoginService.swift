0eaba39415f63591c97d9a5add7a9337bb68b739
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-01-12 23:41:57 -0300

    modules/login/service: Cria service para tela de login

---
+++ News Challenge/News Challenge/modules/login/service/LoginService.swift
@@ -0,0 +1,15 @@
+//
+//  LoginService.swift
+//  News Challenge
+//
+//  Created by Eduardo Herzer on 12/01/21.
+//
+
+import Foundation
+
+class LoginService: LoginServiceProtocol {
+    func signIn(email: String, password: String, success: @escaping (String) -> Void, failure: @escaping (String) -> Void) {
+        //TODO
+        failure("Não implementado")
+    }
+}
