10ebef9adf91c85693fb6ec7c4e4cc435d0fd7ae
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-01-13 20:29:48 -0300

    modules/login/service: Implementa requisição para login

--- News Challenge/News Challenge/modules/login/service/LoginService.swift
+++ News Challenge/News Challenge/modules/login/service/LoginService.swift
@@ -7,9 +7,21 @@
 
 import Foundation
 
-class LoginService: LoginServiceProtocol {
+class LoginService: BaseService, LoginServiceProtocol {
     func signIn(email: String, password: String, success: @escaping (String) -> Void, failure: @escaping (String) -> Void) {
-        //TODO
-        failure("Não implementado")
+        let endpoint = "/v1/client/auth/signin"
+        
+        let parameters = ["email": email,
+                          "password": password]
+        
+        request(endpoint: endpoint, method: .post, parameters: parameters) { (jsonResponse) in
+            if let token = jsonResponse?["token"].string {
+                success(token)
+            } else {
+                failure("Não recebeu o token")
+            }
+        } failure: { (errorMessage) in
+            failure(errorMessage ?? "Não foi possível realizar o login")
+        }
     }
 }
