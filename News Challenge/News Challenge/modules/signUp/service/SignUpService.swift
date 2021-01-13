a5819fed683269631e60a867c4a162ac0a5e7bb1
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-01-12 23:43:30 -0300

    modules/signUp/service: Cria service para tela de cadastro

---
+++ News Challenge/News Challenge/modules/signUp/service/SignUpService.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpService.swift
+//  News Challenge
+//
+//  Created by Eduardo Herzer on 12/01/21.
+//
+
+import Foundation
+
+class SignUpService: SignUpServiceProtocol {
+    func signUp(name: String, email: String, bithDate: String?, password: String, success: @escaping (String) -> Void, failure: @escaping (String) -> Void) {
+        //TODO
+        failure("Não implementado")
+    }
+}
