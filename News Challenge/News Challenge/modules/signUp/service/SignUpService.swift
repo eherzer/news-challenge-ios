c281336b951f24da0293f4f8bda4df0f1f013868
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-01-13 20:30:18 -0300

    modules/signUp/service: Implementa requisição para cadastro de conta

--- News Challenge/News Challenge/modules/signUp/service/SignUpService.swift
+++ News Challenge/News Challenge/modules/signUp/service/SignUpService.swift
@@ -7,9 +7,22 @@
 
 import Foundation
 
-class SignUpService: SignUpServiceProtocol {
+class SignUpService: BaseService, SignUpServiceProtocol {
     func signUp(name: String, email: String, bithDate: String?, password: String, success: @escaping (String) -> Void, failure: @escaping (String) -> Void) {
-        //TODO
-        failure("Não implementado")
+        let endpoint = "/v1/client/auth/signup"
+        
+        let parameters = ["name": name,
+                          "email": email,
+                          "password": password]
+        
+        request(endpoint: endpoint, method: .post, parameters: parameters) { (jsonResponse) in
+            if let token = jsonResponse?["token"].string {
+                success(token)
+            } else {
+                failure("Não recebeu o token")
+            }
+        } failure: { (errorMessage) in
+            failure(errorMessage ?? "Não foi possível realizar o cadastro")
+        }
     }
 }
