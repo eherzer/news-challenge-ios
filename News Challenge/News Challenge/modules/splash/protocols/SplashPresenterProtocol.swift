88e32b9a974dc123fb9bce918dab69f9df445b53
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-01-12 23:35:25 -0300

    modules/splash/protocols: Cria protocolos para tela de splash

---
+++ News Challenge/News Challenge/modules/splash/protocols/SplashPresenterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  SplashPresenterProtocol.swift
+//  News Challenge
+//
+//  Created by Eduardo Herzer on 11/01/21.
+//
+
+import Foundation
+
+protocol SplashPresenterProtocol {
+    func checkAccessToken()
+}
