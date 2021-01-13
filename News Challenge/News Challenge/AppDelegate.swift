81233a28b7cd653e94c414096a2dc71699298c39
Eduardo Herzer <eduardo.herzer@gmail.com>
2021-01-12 23:40:32 -0300

    appDelegate: Utiliza tela de splash como padrão

--- News Challenge/News Challenge/AppDelegate.swift
+++ News Challenge/News Challenge/AppDelegate.swift
@@ -9,8 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let initialViewController = SplashVC()
+        
+        window!.rootViewController = initialViewController
+        window!.makeKeyAndVisible()
+        
         return true
     }
 }
